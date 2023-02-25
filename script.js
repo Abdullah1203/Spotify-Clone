9cbe1705fa84d41d74af53e97c4c10f5682fdbf7
Abdullah1203 <abdullah.ibneali152pro@gmail.com>
2023-02-26 00:53:08 +0600

    All songs play/pause individually

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-console.log("Welcome to spotify clone");
+console.log("Welcome to spotify clone built by ABD NIMIT");
 
 //Initialize the variables
 
@@ -8,19 +8,26 @@ let audioElement = new Audio('song/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
 
 let songs = [
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "song2" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Warriyo - Mortals (feat.pp)" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "song2" , filePath: "song/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "song3" , filePath: "song/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "song4" , filePath: "song/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "song5" , filePath: "song/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "song6" , filePath: "song/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "song7" , filePath: "song/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "song8" , filePath: "song/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "song9" , filePath: "song/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "song10" , filePath: "song/10.mp3", coverPath: "covers/10.jpg"},
 ]
+
+songItems.forEach((element, i)=> {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 
 // audioElement.play();
 
@@ -49,4 +56,25 @@ audioElement.addEventListener('timeupdate', ()=>{
 
 myProgressBar.addEventListener('change', ()=>{
     audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove("fa-pause-circle");
+        element.classList.add("fa-play-circle");
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{
+        makeAllPlays();
+        index = parseInt(e.target.id);
+        e.target.classList.remove("fa-play-circle");
+        e.target.classList.add("fa-pause-circle");
+        audioElement.src = `song/${index+1}.mp3`;
+        audioElement.currenTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+
+    })
 })
