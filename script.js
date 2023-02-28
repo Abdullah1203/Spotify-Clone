4ff65143be4d4af713d59fca7ac37f6855b7f7b8
Abdullah1203 <abdullah.ibneali152pro@gmail.com>
2023-03-01 03:06:33 +0600

    Volume button , favorite button, song album added. New songs added. Ui designed for new Audio player (footer). All complete for now!

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-console.log("Welcome to spotify clone built by ABD NIMIT");
+console.log("Welcome to Spotify (Simple Music Player) clone built by ABD NIMIT");
 
 //Initialize the variables
 
 let songIndex = 0;
 let audioElement = new Audio('song/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
+let mute = document.getElementById('mute');
+let volume = document.getElementById("volume-slider");
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let cover = document.getElementById('cover');
 let currentSongName = document.getElementById('currentSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 
 let songs = [
     {songName: "Iraaday - Abdul Hannan & Rovalio" , filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "The Weeknd - Call Out My Name" , filePath: "song/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "The Weeknd - Blinding Lights" , filePath: "song/2.mp3", coverPath: "covers/2.jpg"},
     {songName: "Twenty one pilots- Stressed Out" , filePath: "song/3.mp3", coverPath: "covers/3.jpg"},
     {songName: "The Chainsmokers - Young (Lyric)" , filePath: "song/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Gotye - Somebody That I Used To Know" , filePath: "song/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "song6" , filePath: "song/6.mp3", coverPath: "covers/6.jpg"},
-    {songName: "song7" , filePath: "song/7.mp3", coverPath: "covers/7.jpg"},
-    {songName: "song8" , filePath: "song/8.mp3", coverPath: "covers/8.jpg"},
-    {songName: "song9" , filePath: "song/9.mp3", coverPath: "covers/9.jpg"},
-    {songName: "song10" , filePath: "song/10.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Gotye - Somebody I Used To Know" , filePath: "song/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "LEVEL FIVE - 60's LOVE" , filePath: "song/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "真夜中のドア (Stay With Me) Cover" , filePath: "song/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "(브라더수) - You don`t know me" , filePath: "song/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Jung Kook- Dreamers مترجمة" , filePath: "song/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Stephen - Until I Found You" , filePath: "song/10.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element, i)=> {
@@ -74,6 +77,7 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
         e.target.classList.add("fa-pause-circle");
         audioElement.src = `song/${songIndex+1}.mp3`;
         currentSongName.innerText = songs[songIndex].songName;
+        cover.src = songs[songIndex].coverPath;
         gif.style.opacity = 1;
         audioElement.currenTime = 0;
         audioElement.play();
@@ -98,11 +102,12 @@ document.getElementById('next').addEventListener('click', ()=>{
     }
     audioElement.src = `song/${songIndex+1}.mp3`;
     currentSongName.innerText = songs[songIndex].songName;
+    cover.src = songs[songIndex].coverPath;
     audioElement.currenTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
-    
+    makeAllPlays();
     
 })
 
@@ -117,9 +122,66 @@ document.getElementById('previous').addEventListener('click', ()=>{
     }
     audioElement.src = `song/${songIndex+1}.mp3`;
     currentSongName.innerText = songs[songIndex].songName;
+    cover.src = songs[songIndex].coverPath;
     audioElement.currenTime = 0;
     audioElement.play();
     masterPlay.classList.remove('fa-play-circle');
     masterPlay.classList.add('fa-pause-circle');
+    makeAllPlays();
 })
 
+//  ----------Volume slider-----------
+
+
+volume.addEventListener("change", function(e) {
+audioElement.volume = e.currentTarget.value / 100;
+})
+
+// ------------Volume Mute/Unmute------------------------
+
+
+mute.addEventListener('click', ()=>{
+    if(audioElement.muted == false){
+        audioElement.muted = true;
+        mute.classList.remove('fa-volume-high');
+        mute.classList.add('fa-volume-xmark');
+    }
+    else{
+        audioElement.muted = false;
+        mute.classList.remove('fa-volume-xmark');
+        mute.classList.add('fa-volume-high'); 
+    }
+})
+// -------------------Transition-------------------
+function toogleLove(x) {
+    x.classList.toggle("fa-solid");
+  }
+// ------------Audio durations--------------
+
+  
+  audioElement.addEventListener('timeupdate', ()=>{
+    var minutes = parseInt(audioElement.duration / 60, 10);
+    var seconds = parseInt(audioElement.duration % 60);
+
+    if (minutes < 10) {
+        minutes = '0' + String(minutes);
+      }
+      if (seconds < 10) {
+        seconds = '0' + String(seconds);
+      }
+    document.getElementById('Audio_durationTime').innerText = minutes + ':' + seconds;
+    // console.log(minutes + ':' + seconds);
+
+    /////Passed time////
+    var mins = Math.floor(audioElement.currentTime / 60);
+    if (mins < 10) {
+      mins = '0' + String(mins);
+    }
+    var secs = Math.floor(audioElement.currentTime % 60);
+    if (secs < 10) {
+      secs = '0' + String(secs);
+    }
+    document.getElementById('Audio_passedTime').innerText = mins + ':' + secs;
+
+	
+})
